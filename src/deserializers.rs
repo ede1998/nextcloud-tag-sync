0b6b0f2552c9d7f3f61839193936ac5ec3e2a044
ede1998 <online@erik-hennig.me>
2023-03-29 23:57:41 +0200

    can read xml with tag list

---
+++ src/deserializers.rs
@@ -0,0 +1,66 @@
+use crate::map::BidirectionalMap;
+
+pub fn deserialize_tags(
+    input: &str,
+) -> Result<BidirectionalMap<u64, String>, Box<dyn std::error::Error>> {
+    let deserializer = &mut quick_xml::de::Deserializer::from_str(input);
+    let element: MultiStatus = serde_path_to_error::deserialize(deserializer)?;
+    Ok(element
+        .props
+        .into_iter()
+        .filter_map(|prop| {
+            let visible = prop.user_visible.unwrap_or_default();
+            let assignable = prop.user_assignable.unwrap_or_default();
+            if !visible || !assignable {
+                return None;
+            }
+
+            prop.id.zip(prop.display_name)
+        })
+        .collect())
+}
+
+#[derive(Debug, serde_query::Deserialize)]
+struct MultiStatus {
+    #[query(".response.[].propstat.prop")]
+    props: Vec<Prop>,
+}
+
+#[derive(serde::Deserialize, Debug, Clone)]
+#[serde(rename_all = "kebab-case")]
+struct Prop {
+    #[serde(deserialize_with = "empty_as_none")]
+    id: Option<u64>,
+    display_name: Option<String>,
+    #[serde(deserialize_with = "empty_as_none")]
+    user_visible: Option<bool>,
+    #[serde(deserialize_with = "empty_as_none")]
+    user_assignable: Option<bool>,
+}
+
+fn empty_as_none<'de, D, T>(de: D) -> Result<Option<T>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+    T: std::str::FromStr,
+    T::Err: std::fmt::Display,
+{
+    match serde::Deserialize::deserialize(de)? {
+        None | Some("") => Ok(None),
+        Some(s) => s.parse().map(Some).map_err(serde::de::Error::custom),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn deserialize_all_tags() {
+        let input = include_str!("../helper-scripts/all_tags.xml");
+        let tags = deserialize_tags(input).unwrap();
+        assert_eq!(tags.len(), 237);
+        assert!(tags
+            .iter()
+            .any(|(&id, name)| id == 73 && name == "Architecture"))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+mod deserializers;
+mod map;
+
+#[tokio::main]
+async fn main() {
+    async fn m() -> Result<(), Box<dyn Error>> {
+        let tags = load_tags_from_network().await;
+        let tags = deserializers::deserialize_tags(&tags)?;
+
+        println!("{tags}");
+        Ok(())
+    }
+    match m().await {
+        Ok(()) => {}
+        Err(e) => println!("{e}"),
+    }
 }
+
+async fn load_tags_from_network() -> String {
+    let client = reqwest::Client::new();
+    client
+        .request(
+            reqwest::Method::from_bytes(b"PROPFIND").unwrap(),
+            "https://cloud.erik-hennig.me/remote.php/dav/systemtags",
+        )
+        .basic_auth(USER, Some(TOKEN))
+        .body(
+            "<?xml version=\"1.0\" encoding=\"utf-8\" ?>
+                <a:propfind xmlns:a=\"DAV:\" xmlns:oc=\"http://owncloud.org/ns\">
+	            <a:prop>
+	                <oc:display-name/>
+	                <oc:user-visible/>
+	                <oc:user-assignable/>
+	                <oc:id/>
+        	    </a:prop>
+        	</a:propfind>",
+        )
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap()
+}
+
+const USER: &str = "erik";
+const TOKEN: &str = include_str!("../helper-scripts/nextcloud-token.txt");

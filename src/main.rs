7e05b73d3b2d0e5f8295bbd417fe2b8a32248f3c
ede1998 <online@erik-hennig.me>
2023-03-31 19:41:02 +0200

    add async inotify watch

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod map;
 mod requests;
 
 #[tokio::main]
-async fn main() {
+async fn main1() {
     async fn m() -> Result<(), Box<dyn Error>> {
         let connection = Connection::default();
         let tags = connection.request(ListTags).await?;
@@ -21,4 +21,52 @@ async fn main() {
         Ok(()) => {}
         Err(e) => println!("{e}"),
     }
+}
+
+use futures::{channel::mpsc::channel, SinkExt, StreamExt};
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+use std::path::Path;
+
+#[tokio::main]
+async fn main() {
+    let path = "/home/erik/Documents";
+    futures::executor::block_on(async {
+        if let Err(e) = async_watch([path]).await {
+            println!("error: {:?}", e)
+        }
+    });
+}
+
+async fn async_watch<P, I>(paths: I) -> notify::Result<()>
+where
+    P: AsRef<Path>,
+    I: IntoIterator<Item = P>,
+{
+    let (mut tx, mut rx) = channel(100);
+
+    let mut watcher = RecommendedWatcher::new(
+        move |res| {
+            futures::executor::block_on(async {
+                if let Err(e) = tx.send(res).await {
+                    if e.is_full() {
+                        println!("Dropping event because queue is full.");
+                    }
+                }
+            })
+        },
+        Config::default(),
+    )?;
+
+    for path in paths {
+        watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
+    }
+
+    while let Some(res) = rx.next().await {
+        match res {
+            Ok(event) => println!("changed: {:?}", event),
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+
+    Ok(())
 }
